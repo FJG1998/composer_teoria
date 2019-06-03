c4cf66388afa6b0fc62cca429661aa082f8d9159
fjimenezjob@gmail.com <franjimenez@hotelbeds.com>
2019-06-03 15:18:56 +0200

    composer integrado y head creado y listo

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+$header = new models\Header('es','UTF-8','pruebas','ESTO ES LA DESCRIPCION DE LA PAGINA','IMAGEN','URL');
+
+$header->printHead();
+
+?>
+
+<body>
+
+<!-- CONTENIDO -->
+<h1>HOLA TODO FUNCIONA BIEN, SALGO PORQUE ERES UN CRUCK Y HAN MUERTO 20000 NEURONAS DE TU CABEZA PARA QUE PUDIERA SALIR</h1>
+
+<!-- ************************** -->
+
+</body>
+
+</html>
---
+++ models/Footer.php
@@ -0,0 +1,8 @@
+<?php namespace models;
+
+require_once 'vendor/autoload.php';
+
+class footer{
+
+    
+}
